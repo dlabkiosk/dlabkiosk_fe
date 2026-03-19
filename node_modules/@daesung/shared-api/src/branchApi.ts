4e964550bc9b5892413add6462ce65b7c6bc476d
unknown <kimkyungjin@signal-decode.com>
2026-03-19 16:03:15 +0900

    api 정리

--- node_modules/@daesung/shared-api/src/branchApi.ts
+++
@@ -1,7 +0,0 @@
-import { dsaAuthFetch } from './dsaClient.ts';
-import type { GetDlabListResponse } from './types/dsa.types.ts';
-
-/** 지점(DLab) 목록 조회 */
-export function getDlabList(): Promise<GetDlabListResponse> {
-  return dsaAuthFetch('/kiosk/getDlabList');
-}
